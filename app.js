55000042c6325825c6f0528f5666c5e03a0db9b6
Florian Fougeray <fougeray.florian@gmail.com>
2026-05-02 11:48:50 +0200

    project init

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import expressLayouts from 'express-ejs-layouts';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.set('layout', 'layout/main');
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home', { title: "Gay'vent" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
